dd53bd43b8a3cab9587205c0375900622bce9c01
unknown <tiagocvalim@gmail.com>
2020-06-18 09:07:26 -0300

    Dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ifood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
